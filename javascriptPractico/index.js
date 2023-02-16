bc7817a4c6ea0d8fc9603184e9f55b7fcad073d2
Carlos Bahena <carlosbahmar@gmail.com>
2023-02-16 17:12:00 -0600

    segunda sesion, query selector

--- javascriptPractico/index.js
+++ javascriptPractico/index.js
@@ -1,1 +1,23 @@
-console.log('Hello, World');
+
+
+const h1 = document.querySelector('h1');
+const p = document.querySelectorAll('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+
+
+// la funcuon "document" se utiliza para hacer referencia al html
+// el queryselector nos permite seleccionar clases, etiquetas o id haciendo como argumento el elemento sobre e que se quiere referir la variable
+//el queryselector selecciona solo el primer elemento que encuentre de su argumento, el queryselectorall crea un array de todos los elementos que contengan el argumento, si solo es uno, se genera un array con un elemento
+
+console.log(input.value);
+
+
+console.log( {
+  h1,
+  p,
+  parrafito,
+  pid,
+  input
+});
